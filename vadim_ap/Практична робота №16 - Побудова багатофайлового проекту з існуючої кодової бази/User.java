5110ed2fc3f88cb321abec3c7e11424ab598b390
Xeone52670v3 <152479320+Xeone52670v3@users.noreply.github.com>
2025-06-08 15:34:30 +0300

    Delete vadim_ap/Практична робота №16 - Побудова багатофайлового проекту з існуючої кодової бази

--- vadim_ap/Практична робота №16 - Побудова багатофайлового проекту з існуючої кодової бази/User.java
+++
@@ -1,9 +0,0 @@
-public class User {
-    String name;
-    String password;
-
-    public User(String name, String password) {
-        this.name = name;
-        this.password = password;
-    }
-}
