65d9cd2d74eeb69c34e575d967ae003669ebbf02
Xeone52670v3 <152479320+Xeone52670v3@users.noreply.github.com>
2025-06-08 15:35:16 +0300

    Add files via upload

---
+++ vadim_ap/Практична робота №16 - Побудова багатофайлового проекту з існуючої кодової бази/Base.java
@@ -0,0 +1,10 @@
+package game;
+
+import java.util.Scanner;
+
+public class Base {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
+}
---
+++ vadim_ap/Практична робота №16 - Побудова багатофайлового проекту з існуючої кодової бази/Config.java
@@ -0,0 +1,5 @@
+package game;
+
+public class Config {
+    public static int boardSize = 3;
+}
