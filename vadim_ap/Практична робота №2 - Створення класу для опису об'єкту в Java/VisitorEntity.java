efcdc4c691023c579d941e66f2ab8c8eb4eed029
Xeone52670v3 <152479320+Xeone52670v3@users.noreply.github.com>
2025-10-10 18:09:34 +0300

    VisitorEntity.java

--- vadim_ap/Практична робота №2 - Створення класу для опису об'єкту в Java/VisitorEntity.java
+++ vadim_ap/Практична робота №2 - Створення класу для опису об'єкту в Java/VisitorEntity.java
@@ -1,22 +1,45 @@
-public class VisitorEntity {
+public class Visitor {
+    private int visitorId;
     private String visitorName;
+    private String visitorContactPhone;
     private String visitorGoal;
+    private String[] visitorOrder;
+    private String[] visitorPreferences;
+    private String visitorPaymentMethod;
+    private String visitorFeedback;
+    private int visitorLoyaltyPoints;
 
-    public VisitorEntity(String visitorName, String visitorGoal) {
+    public Visitor(int visitorId, String visitorName, String visitorContactPhone, String visitorGoal,
+                   String[] visitorOrder, String[] visitorPreferences, String visitorPaymentMethod,
+                   String visitorFeedback, int visitorLoyaltyPoints) {
+        this.visitorId = visitorId;
         this.visitorName = visitorName;
+        this.visitorContactPhone = visitorContactPhone;
         this.visitorGoal = visitorGoal;
+        this.visitorOrder = visitorOrder;
+        this.visitorPreferences = visitorPreferences;
+        this.visitorPaymentMethod = visitorPaymentMethod;
+        this.visitorFeedback = visitorFeedback;
+        this.visitorLoyaltyPoints = visitorLoyaltyPoints;
     }
 
-    public String getVisitorName() {
-        return visitorName;
-    }
-
-    public String getVisitorGoal() {
-        return visitorGoal;
-    }
+    // Порожні методи-заглушки
+    public void makeOrder() {}
+    public void giveFeedback() {}
+    public void useLoyaltyPoints() {}
 
     @Override
     public String toString() {
-        return "Відвідувач{ім'я='" + visitorName + "', мета='" + visitorGoal + "'}";
+        return "Visitor{" +
+                "id=" + visitorId +
+                ", name='" + visitorName + '\'' +
+                ", phone='" + visitorContactPhone + '\'' +
+                ", goal='" + visitorGoal + '\'' +
+                ", paymentMethod='" + visitorPaymentMethod + '\'' +
+                ", feedback='" + visitorFeedback + '\'' +
+                ", loyaltyPoints=" + visitorLoyaltyPoints +
+                '}';
     }
 }
+
+
