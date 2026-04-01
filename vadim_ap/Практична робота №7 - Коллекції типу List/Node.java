8521da738c4703591e2dc109705c95b6ab79eb48
Xeone52670v3 <152479320+Xeone52670v3@users.noreply.github.com>
2026-04-01 20:07:10 +0300

    Add files via upload

---
+++ vadim_ap/Практична робота №7 - Коллекції типу List/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    Object data;
+    Node next;
+    Node prev;
+
+    public Node(Object data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
